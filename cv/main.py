79da116a0af3cd563d42594807df759aa07bfe0b
William Lindholm <william_lindholm@outlook.com>
2026-09-25 00:23:22 +0200

    loosened security headers to allow cloudflare analytics

--- cv/main.py
+++ cv/main.py
@@ -11,10 +11,9 @@ app = Flask(__name__, static_folder=None, template_folder=".")
 @app.after_request
 def security_headers(response):
     response.headers["Content-Security-Policy"] = (
-        "default-src 'none'; style-src 'self'; img-src 'self'; "
-        "script-src https://cloudflare.com https://*.cloudflare.com "
-        "https://cloudflareinsights.com https://*.cloudflareinsights.com; "
-        "connect-src https://cloudflareinsights.com https://*.cloudflareinsights.com; "
+        "default-src 'self'; style-src 'self'; img-src 'self'; "
+        "script-src 'self' https: 'unsafe-inline'; "
+        "connect-src 'self' https:; frame-src 'self' https:; object-src 'none'; "
         "base-uri 'none'; form-action 'none'; frame-ancestors 'none'"
     )
     response.headers["Strict-Transport-Security"] = "max-age=31536000"
